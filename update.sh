5eb85b90cbf7bb3e46197dd9d07a17c8d63ab45f
Kevin de Vos <kevin.de.vos@larkinfolab.nl>
2024-03-01 11:14:12 +0100

    add script to update patches from current install

---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TALON_DIR="/Users/kevinlark/.talon/user"
+MY_TALON="/Users/kevinlark/Documents/my_talon/"
+
+# Update diff patches
+cd "${TALON_DIR}/community"
+git diff > "${MY_TALON}/community.diff"
+
+cd "${TALON_DIR}/webspeech_examples"
+git diff > "${MY_TALON}/webspeech_examples.diff"
+
+cd "${TALON_DIR}/rango-talon"
+git diff > "${MY_TALON}/rango.diff"
